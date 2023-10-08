bdbd5b281a2bac331a6dd47c3150507313389e5f
Matthew <Matirix@live.com>
2023-10-07 17:25:16 -0700

    done

--- app.js
+++ app.js
@@ -17,10 +17,8 @@ class WordDefinition {
     }
 } 
 
-const wordddd = new WordDefinition("hello", "a greeting")
-const dictionary = [
-    wordddd
-];
+const dictionary = [];
+let requestNum = 0;
 
 http.createServer((req, res) => {
     const link = url.parse(req.url, true)
@@ -30,38 +28,39 @@ http.createServer((req, res) => {
     if (pathname.startsWith("/api/definitions/")) {
         let qword = query.word ? query.word : null
         let qdef = query.definition ? query.definition : null
+        requestNum++;
 
         if (req.method == 'GET' && qword != null) {
             let found = false;
             dictionary.forEach((word) => {
                 if (word.getWord() == qword) {
                     res.writeHead(200, defaultHeader);
-                    res.end(JSON.stringify({ word: word.word, definition: word.definition }));
+                    res.end(JSON.stringify({ requestNum: `Request #${requestNum}`, word: word.word, definition: word.definition }));
                     found = true;
                 }
             });
             if (!found) {
                 res.writeHead(404, defaultHeader);
-                res.end(JSON.stringify({ error: "Word does not exist!" }));
+                res.end(JSON.stringify({ error: `Request #${requestNum} Word '${qword}' not found!` }));
             }
         } else if (req.method == 'POST' && qword != null && qdef != null) {
             const exists = dictionary.find(word => word.word == qword);
             if (exists !== undefined) {
               res.writeHead(401, defaultHeader);
-              res.end(JSON.stringify({ error: "Word Already Exists!" }));
+              res.end(JSON.stringify({ error: `Request #${requestNum} Word Already Exists!` }));
             } else {
               dictionary.push(new WordDefinition(qword, qdef));
               res.writeHead(200, defaultHeader);
-              res.end(JSON.stringify({ message: `Request #${dictionary.length}`, newEntry: `${qword} : ${qdef}` }));
+              res.end(JSON.stringify({ requestNum: `Request #${requestNum}`, totalWords: `Total words in the dictionary: ${dictionary.length}`, newEntry: `New entry recorded: ${qword}: ${qdef}` }));
               res.end();
             }
         } else {
             res.writeHead(404, defaultHeader)
-            res.end(JSON.stringify({ error: "Invalid Request, Missing word or definition" }));
+            res.end(JSON.stringify({ error: `Request #${requestNum} Invalid Request, Missing word or definition` }));
         }
     } else {
         res.writeHead(404, defaultHeader)
-        res.end(JSON.stringify({ error: "Please go to /api/definitions/ to begin" }));
+        res.end(JSON.stringify({ error: `Request #${requestNum} Please go to /api/definitions/ to begin` }));
     }
     
 }).listen(PORT)
